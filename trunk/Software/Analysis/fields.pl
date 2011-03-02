0138979f10915f758dff4bae6c027fb45a8f779f
shimniok@gmail.com <shimniok@gmail.com@df5af02f-833e-5f0c-5007-46721fc80139>
2011-03-02 16:05:14 +0000

    Include file specifies index of each data field.

---
+++ trunk/Software/Analysis/fields.pl
@@ -0,0 +1,18 @@
+#   0      1     2       3     4      5     6    7        8        9     10     11    12,     13
+# Millis, Gyro, Compass, Lat, Long, Age1, Date, Time, Altitude, Course, Speed, HDOP, Bearing, Distance
+#
+$MILLIS=0;
+$GYRO=1;
+$COMPASS=2;
+$LAT=3;
+$LON=4;
+$AGE1=5;
+$DATE=6;
+$TIME=7;
+$ALT=8;
+$COURSE=9;
+$SPEED=10;
+$HDOP=11;
+$BRG=12;
+$DIST=13;
+
