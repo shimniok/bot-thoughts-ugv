35108cea8d640d93f0c891090a49502b89525741
shimniok@gmail.com <shimniok@gmail.com@df5af02f-833e-5f0c-5007-46721fc80139>
2011-04-27 19:57:18 +0000

    As run on April 23 2011

---
+++ Analysis/lib/fields.pl
@@ -0,0 +1,21 @@
+#   0      1        2              3            4         5        6     7     8      9        10,     11,   12,     13,     14,        15,           16
+# Millis, Gyro, GyroHeading, CompassHeading, Latitude, Longitude, Age, Date, Time, Altitude, Course, Speed, HDOP, Bearing, Distance, Left Enc, Right Enc, Gyro Temp
+#
+$MILLIS=0;
+$GYRO=1;
+$GYROHDG=2;
+$COMPASS=3;
+$LAT=4;
+$LON=5;
+$GPSID=6;
+$DATE=7;
+$TIME=8;
+$ALT=9;
+$COURSE=10;
+$SPEED=11;
+$HDOP=12;
+$BRG=13;
+$DIST=14;
+$LENC=16;
+$RENC=17;
+$TEMP=18;
