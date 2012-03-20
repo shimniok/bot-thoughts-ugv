b8a5dc9de87fc61e8cfdaf0346560c83a2b32f68
shimniok@gmail.com <shimniok@gmail.com@df5af02f-833e-5f0c-5007-46721fc80139>
2012-03-20 15:01:08 +0000

    Converted to function taking data filename as argument

--- Software/Compass/Octave/compass_solver.m
+++ Software/Compass/Octave/compass_solver.m
@@ -15,40 +15,46 @@ end
 %				
 % Magnetometer data
 %	
-global M=load("cal000b.csv");
-%
-% initial guess using ellipsoid fit
-%
-source ellipsoid_fit/ellipsoid_fit.m
-[ O R E params ] = ellipsoid_fit(M);
-X0 = [ O(1) O(2) O(3) R(1) R(2) R(3) ];
-%
-% Run the non-linear solver
-%
-[X, obj, info, iter, nf, lambda] = sqp(X0, @phi);
-%
-% Compare O and R guess with solver results
-%
-O
-O2 = [ X(1:3) ]
-R
-R2 = [ X(4:6) ]
-%
-% Let's plot it and see what it looks like
-%
-S2 = diag([100/X(4) 100/X(5) 100/X(6)])
-M2 = (M-ones(length(M),1)*O2')*S2;
-N2 = norm(M2, 2, 'rows');
-close all;
-figure;
-plot(M2(:,1),N2,'.');
-axis([-110 110 80 120]);
-title("X vs Magnitude", "fontsize", 16);
-figure;
-plot(M2(:,2),N2,'.');
-axis([-110 110 80 120]);
-title("Y vs Magnitude", "fontsize", 16);
-figure;
-plot(M2(:,3),N2,'.');
-axis([-110 110 80 120]);
-title("Z vs Magnitude", "fontsize", 16);
+global M;
+
+function Q=compass_solver(file)
+	global M;
+	M=load(file);
+	%
+	% initial guess using ellipsoid fit
+	%
+	source ellipsoid_fit/ellipsoid_fit.m
+	[ O R E params ] = ellipsoid_fit(M);
+	X0 = [ O(1) O(2) O(3) R(1) R(2) R(3) ];
+	%
+	% Run the non-linear solver
+	%
+	[X, obj, info, iter, nf, lambda] = sqp(X0, @phi);
+	%
+	% Compare O and R guess with solver results
+	%
+	O
+	O2 = [ X(1:3) ]
+	R
+	R2 = [ X(4:6) ]
+	%
+	% Let's plot it and see what it looks like
+	%
+	S2 = diag([100/X(4) 100/X(5) 100/X(6)])
+	M2 = (M-ones(length(M),1)*O2')*S2;
+	N2 = norm(M2, 2, 'rows');
+	Q=[ X(1:3); 100/X(4); 100/X(5); 100/X(6) ];
+	close all;
+	figure;
+	plot(M2(:,1),N2,'.');
+	axis([-110 110 80 120]);
+	title("X vs Magnitude", "fontsize", 16);
+	figure;
+	plot(M2(:,2),N2,'.');
+	axis([-110 110 80 120]);
+	title("Y vs Magnitude", "fontsize", 16);
+	figure;
+	plot(M2(:,3),N2,'.');
+	axis([-110 110 80 120]);
+	title("Z vs Magnitude", "fontsize", 16);
+end
