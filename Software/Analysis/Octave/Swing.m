f717b9dfa2dd194594c66495edb0a99fcd803cfb
shimniok@gmail.com <shimniok@gmail.com@df5af02f-833e-5f0c-5007-46721fc80139>
2013-09-11 17:01:39 +0000

    RC_MUX R0.2 as ordered

--- Software/Analysis/Octave/Swing.m
+++
@@ -1,8 +0,0 @@
-function S=swing(file)
-	E=load(file);
-	plot(E(:,2), mod(E(:,2)-E(:,3),180), '.');
-	xlabel("Heading", "fontsize", 16);
-	ylabel("Error", "fontsize", 16);
-	title("Compass Err vs Heading", "fontsize", 20);
-	grid on;
-end
