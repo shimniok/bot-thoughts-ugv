31882818666a73a01457613c4ac1f1af5724386f
shimniok@gmail.com <shimniok@gmail.com@df5af02f-833e-5f0c-5007-46721fc80139>
2013-10-01 14:18:19 +0000

    Reworked detect code to simplify and fix behavior bugs, now all runs in timer ISR.

--- trunk/Software/RC_MUX/main.c
+++ trunk/Software/RC_MUX/main.c
@@ -26,13 +26,8 @@
 #define MINONTIME	300L
 #define MAXONTIME	3000L
 
-#define	TRUE		1
-#define FALSE		0
-
 #define MCUenable	PB3
 #define RCenable	PB4
-#define AUTO		1
-#define MANUAL		0
 
 #define VALIDCNT	3				// number of valid signals required to go manual
 #define DT			100				// delta time for ticks, usec
@@ -40,31 +35,29 @@
 
 /************* GLOBAL VARIABLES **************/
 
-volatile uint8_t mode=AUTO;			// Mode of operation, auto or manual
+uint8_t inactive1=VALIDCNT;			// RX signal active pulse counter
+long ontime1;  						// measure length of signal on time
+long offtime1; 						// measure length of signal off time
+uint8_t history1;					// history of pin states
 
-volatile uint8_t inactive1=VALIDCNT;// RX signal active pulse counter
-volatile long ontime1;  			// measure length of signal on time
-volatile long offtime1; 			// measure length of signal off time
-
-volatile uint8_t inactive3=VALIDCNT;// RX signal valid counter
-volatile long ontime3;  			// measure length of signal on time
-volatile long offtime3; 			// measure length of signal off time
+uint8_t inactive3=VALIDCNT;			// RX signal active pulse counter
+long ontime3;  						// measure length of signal on time
+long offtime3; 						// measure length of signal off time
+uint8_t history3;					// history of pin states
 
 /****************** CODE *********************/
 
 int main() {
-	/* setup pins for output */
+	// setup pins for output
+	// This is in the main routine instead of a function to save a few bytes of flash
 	DDRB = (1<<MCUenable)|(1<<RCenable);
 
-	/* Setup timer to keep track of timing of signal */
+	// Setup timer to keep track of timing of signal
+	// This is in the main routine instead of a function to save a few bytes of flash
 	TCCR0A = (1<<WGM01);					// CTC mode
 	TCCR0B = (0<<CS02)|(1<<CS01)|(0<<CS00); // clk/8, 9.6MHz -> 1.2MHz
 	OCR0A = 111; 							// 1.2MHz / 120 -> 10kHz, 100usec
 	TIMSK0 = (1<<OCIE0A);					// Enable output compare A interrupt
-
-	/* Setup pin interrupts for input signals */
-	PCMSK |= (1<<CH1INT)|(1<<CH3INT);	// Enable interrupt for PB1/PCINT1
-	GIMSK |= (1<<PCIE);					// Enable pin change interrupts
 
 	/* Ready to receive interrupts, now */
 	sei();
@@ -74,67 +67,77 @@ int main() {
 
 
 /**
- * Interupt handler for pin change, detects and tracks RC signal
- */
-ISR(PCINT0_vect) {
-	long p;
-	// Check ontime, period on rising edge
-	if ((PINB & (1<<CH1RX)) != 0) {
-		// If signal period is around 20ms and on-period is ~1-2ms, we have a valid signal
-		p = ontime1+offtime1;
-		if (inactive1 && p > MINPERIOD && p < MAXPERIOD && ontime1 > MINONTIME && ontime1 < MAXONTIME) {
-			--inactive1;
-		}
-		ontime1 = offtime1 = 0;
-	}
-
-	if ((PINB & (1<<CH3RX)) != 0) {
-		p = ontime3+offtime3;
-		if (inactive3 && p > MINPERIOD && p < MAXPERIOD && ontime3 > MINONTIME && ontime3 < MAXONTIME) {
-			--inactive3;
-		}
-		ontime3 = offtime3 = 0;
-	}
-
-	if (!inactive3) {
-		if (ontime3 > 1500) {
-			PORTB = (1<<RCenable);
-		} else {
-			PORTB = (1<<MCUenable);
-		}
-	} else if (!inactive1) {
-		PORTB = (1<<RCenable);
-	}
-
-}
-
-
-/**
- * Timer interrupt handler, fires every ~10us and subtracts from timeout value
+ * Timer interrupt handler, fires periodically, counts on time, off time, and manages signal timeout
  */
 ISR(TIM0_COMPA_vect) {
+	// Count on time and off time for CH1
+	history1 <<= 1;
 	if ((PINB & (1<<CH1RX)) == 0) {
 		offtime1 += DT;  // track off time
+		// Does the signal off time exceeds the timeout?
 		if (offtime1 > TIMEOUT) {
 			inactive1 = VALIDCNT;
-			ontime1 = offtime1 = 0;
 		}
 	} else {
 		ontime1 += DT;   // track on time
+		history1 |= 1;
 	}
 
+	long p;
+	// Positive edge
+	if (history1 == 0b00000001) {
+		p = ontime1+offtime1;
+		if (inactive1 && p > MINPERIOD && p < MAXPERIOD && ontime1 > MINONTIME && ontime1 < MAXONTIME) {
+			// Enable RC if CH1 active but CH3 inactive
+			if ((--inactive1 == 0) && inactive3) {
+				PORTB = (1<<RCenable);
+			}
+		}
+		ontime1 = 0;
+	}
+	// Negative edge
+	if (history1 == 0b11111110) {
+		offtime1 = 0;
+	}
+
+	// Count on time and off time for CH3
+	history3 <<= 1;
 	if ((PINB & (1<<CH3RX)) == 0) {
 		offtime3 += DT;  // track off time
+		// Does the signal off time exceeds the timeout?
 		if (offtime3 > TIMEOUT) {
 			inactive3 = VALIDCNT;
-			ontime3 = offtime1 = 0;
 		}
 	} else {
 		ontime3 += DT;   // track on time
+		history3 |= 1;
 	}
 
+	// Positive edge
+	if (history3 == 0b00000001) {
+		p = ontime3+offtime3;
+		if (inactive3 && p > MINPERIOD && p < MAXPERIOD && ontime3 > MINONTIME && ontime3 < MAXONTIME) {
+			--inactive3; // using a single variable to conserve program memory
+		}
+		// Enable RC if CH3 active and CH3 on time > 1.5ms, else disable RC
+		if (!inactive3) {
+			if (ontime3 > 1500) {
+				PORTB = (1<<RCenable);
+			} else {
+				PORTB = (1<<MCUenable);
+			}
+		}
+		ontime3 = offtime3 = 0;
+	}
+	// Negative edge
+	if (history3 == 0b11111110) {
+		offtime3 = 0;
+	}
+
+	// If both CH1 and CH3 have timed out, disable RC
 	if (inactive1 && inactive3) {
 		PORTB = (1<<MCUenable);
 	}
 }
+
 
